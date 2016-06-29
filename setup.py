966b76f7eb08bd968c5cb9704ee973d825470165
Mike Boers <mikeb@markmedia.co>
2016-06-29 11:11:14 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mmpipeline',
+    version='0.1.dev0',
+    description='Mark Media\'s general pipeline tools',
+    url='http://github.com/mmpipeline/mmpipeline',
+    
+    packages=find_packages(exclude=['build*', 'tests*']),
+    
+    author='Mike Boers',
+    author_email='mmpipeline@mikeboers.com',
+    license='BSD-3',
+    
+    scripts=[
+        'scripts/fix-firefox',
+    ],
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    
+)
